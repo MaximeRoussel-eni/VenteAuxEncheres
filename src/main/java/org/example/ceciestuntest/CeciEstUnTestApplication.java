ace26263dc72315167ee84fff85aa987e49be1db
JabilBoquet <erwin.leboulanger@gmail.com>
2025-01-27 13:30:02 +0100

    Ceci est un test

---
+++ src/main/java/org/example/ceciestuntest/CeciEstUnTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.ceciestuntest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeciEstUnTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CeciEstUnTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ceciestuntest/CeciEstUnTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ceciestuntest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeciEstUnTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
