44967e1ad23d807c18b0e1def305beefef15d2be
JabilBoquet <erwin.leboulanger@gmail.com>
2025-01-29 15:56:32 +0100

    On peut lire et faire remonter les catégories

---
+++ src/main/java/application/dal/CategorieDaoImpl.java
@@ -0,0 +1,32 @@
+package application.dal;
+
+import application.bo.Categorie;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public class CategorieDaoImpl implements CategorieDao {
+
+    private final String READ_CATEGORY_BY_NO_CATEGORIE = "SELECT * FROM CATEGORIES WHERE NO_CATEGORIE = :noCategorie";
+    private final String READ_ALL_CATEGORIES = "SELECT * FROM CATEGORIES";
+
+    @Autowired
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    @Override
+    public Categorie getCategorieById(int noCategorie) {
+        MapSqlParameterSource namedParameters = new MapSqlParameterSource();
+        namedParameters.addValue("noCategorie", noCategorie);
+        return namedParameterJdbcTemplate.queryForObject(READ_CATEGORY_BY_NO_CATEGORIE,namedParameters, new BeanPropertyRowMapper<>(Categorie.class));
+    }
+
+    @Override
+    public List<Categorie> getAllCategories() {
+        return  namedParameterJdbcTemplate.query(READ_ALL_CATEGORIES, BeanPropertyRowMapper.newInstance(Categorie.class));
+    }
+}
